0bab32dce61493dedbac352dc6282e5f48d6aab8
garethrowell <gar417@gmail.com>
2025-03-14 14:28:33 -0500

    python version

--- QCingMonitoringData.R
+++ QCingMonitoringData.R
@@ -1,22 +1,79 @@
 
+# -----------------------
+# QCing monitoring data
+# R tidyverse version
+# Gareth Rowell 2/14/2025
+# -----------------------
+
 
 library(tidyverse)
+library(lubridate)
+
+# Step 1 - loading the data
 
 setwd("C:/Users/Growell/QC-tips-and-tricks")
 getwd()
 	
 df <- read_csv("BirdObservationsThru2022_3.csv")
-df
-	
-head(df)
-tail(df)
-	
-view(df)
-	
+
 glimpse(df)
+	
 
-# Here, I am selecting a small subset of data to get started...
+# Step 2 - selecting data 
 
-df |> select(Make, Model, Model_Year)
+df |> select(ParkUnit, EventDateTime, Temperature_C,  Rain, CommonName, Distance) |> view()
+
+
+# Step 3 - create unique lists for text data
+
+df |> distinct(ParkUnit) 
+
+df |> distinct(CommonName) |> 
+   arrange(CommonName) |>
+   print(n = 223) 
+   
+
+# Step 4 - create group by counts for categorical data
+
+df |> group_by(Rain) |> count() 
+
+
+# Step 5 - create bar charts for dates and numerical columns
+
+df2 <- df |> select(EventDateTime, Temperature_C, Distance) |>
+	filter(Temperature_C > -9999) |>
+	filter(Distance > -9999) |>
+    mutate(
+	obs_date = as.Date(EventDateTime),
+	obs_time = hms::as_hms(EventDateTime)
+	)
+	
+ggplot(df2, aes(x = obs_date)) +
+    geom_histogram()
+	
+ggplot(df2, aes(x = obs_time)) +
+    geom_histogram()	
+	
+ggplot(df2, aes(x = Temperature_C)) +
+    geom_histogram()
+
+ggplot(df2, aes(x = Distance)) +
+    geom_histogram()
+
+	
+	
+
+
+	
+
+
+
+
+
+
+
+
+
+
 	
 	
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # QC-tips-and-tricks
 
-This repo contains a couple of very simple scripts for establishing quality control (QC'ing) over datasets. I've included two scripts, one in R tidyverse and the other in Python pandas.  These scripts are based on the following five steps to quickly locate problems in datasets: (1) load data, (2) select columns, (3) create unique lists for text data, (4) create group by counts for categorical data and (5) create bar charts for date, integer and continuous numeric data. The bar charts for date and integer are counts histograms while the bar chart for continuous data is a binned continuous histogram. Everything else I do for QCind data is just an extension of these five steps.
+This repo contains a couple of scripts for establishing quality control (QC'ing) over datasets. There's one in R tidyverse and the other is in Python pandas.  I basically use five steps for most of my QC work: (1) load data, (2) select columns, (3) create unique lists for text data, (4) create group by counts for categorical data and (5) create histograms for numerical and dates. I re-use these basic operations over and over again.
 
 The example data is an ecological monitoring dataset containing bird point counts by David Peitz and coworkers. It is available here: https://doi.org/10.57830/2300777
 
